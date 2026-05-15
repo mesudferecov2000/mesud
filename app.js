76d3d8b8286571831a81aeb682291d48dea77711
mesudferecov2000 <mesudferecov200@gmail.com>
2026-05-15 16:04:51 +0400

    JAVA23

--- app.js
+++ app.js
@@ -1,24 +1,59 @@
-let arr = ['green', 'red', 'blue'];
+let arr1 = [10, 2, 45, 6, 9, 78, 36];
 
-let result = arr.find(function (i) {
-    if (i.length == 3) {
-        return i;
-    }
+
+let sum = arr1.reduce(function(total, num) {
+    return total + num;
+}, 0);
+
+console.log("Cəm:", sum);
+
+ 
+
+let arr2 = ["Anara", "Kamal", "Kenan"];
+
+let longest = arr2.reduce(function(a, b) {
+    return a.length < b.length ? a : b;
 });
 
-console.log(result);
+console.log("Ən uzun söz:", longest);
 
-let arr2 = ['Abbas', 'Amin', 'Vaqif', 'Ferhad', 'Anar'];
 
-let result2 = arr2.filter(function (a) {
-    if (a[0] == 'A') {
-        return a;
-    }
+let arr3 = ["anar", "kamal", "kenan"];
+
+let upperCaseArr = arr3.map(function(word) {
+    return word.toUpperCase();
 });
 
-console.log(result2);
+console.log("Böyük hərflə:", upperCaseArr);
 
 
+let arr4 = [10, 2, 6, 9];
+
+let squares = arr4.map(function(num) {
+    return num * num;
+});
+
+console.log("Kvadratları:", squares);
+
+
+
+let arr5 = [10, 2, 6, 9];
+
+let sorted = arr5.sort(function(a, b) {
+    return b - a;
+});
+
+console.log("İkinci ən böyük:", sorted[1]);
+
+
+
+let arr6 = [10, 10, 10, 10];
+
+let allSame = arr6.every(function(num) {
+    return num === arr6[0];
+});
+
+console.log("Bütün elementlər eynidir?", allSame);
 
 
 
