94fe3f343c23961d97019ca0e29d21ccedc232dc
mesudferecov2000 <mesudferecov200@gmail.com>
2026-06-05 19:12:47 +0400

    tapsiriqqq

--- app.js
+++ app.js
@@ -1,216 +1,87 @@
-// let arr1 = [10, 2, 45, 6, 9, 78, 36];
+const tbody = document.getElementById("tbody");
+const addBtn = document.getElementById("addBtn");
 
+addBtn.addEventListener("click", () => {
 
-// let sum = arr1.reduce(function(total, num) {
-//     return total + num;
-// }, 0);
+    const name = document.getElementById("name").value;
+    const surname = document.getElementById("surname").value;
+    const salary = document.getElementById("salary").value;
 
-// console.log("Cəm:", sum);
+    if(name === "" || surname === "" || salary === ""){
+        alert("Butun xanalari doldurun!");
+        return;
+    }
 
- 
+    const tr = document.createElement("tr");
 
-// let arr2 = ["Anara", "Kamal", "Kenan"];
+    tr.innerHTML = `
+        <td></td>
+        <td>${name}</td>
+        <td>${surname}</td>
+        <td>${salary}</td>
+        <td>
+            <button class="delete">Sil</button>
+            <button class="edit">Düzəliş et</button>
+        </td>
+    `;
 
-// let longest = arr2.reduce(function(a, b) {
-//     return a.length < b.length ? a : b;
-// });
+    tbody.appendChild(tr);
 
-// console.log("Ən uzun söz:", longest);
+    updateNumbers();
 
+    document.getElementById("name").value = "";
+    document.getElementById("surname").value = "";
+    document.getElementById("salary").value = "";
+});
 
-// let arr3 = ["anar", "kamal", "kenan"];
+tbody.addEventListener("click", (e) => {
 
-// let upperCaseArr = arr3.map(function(word) {
-//     return word.toUpperCase();
-// });
+    if(e.target.classList.contains("delete")){
+        e.target.closest("tr").remove();
+        updateNumbers();
+    }
 
-// console.log("Böyük hərflə:", upperCaseArr);
+    if(e.target.classList.contains("edit")){
 
+        const row = e.target.closest("tr");
 
-// let arr4 = [10, 2, 6, 9];
+        const name = row.children[1];
+        const surname = row.children[2];
+        const salary = row.children[3];
 
-// let squares = arr4.map(function(num) {
-//     return num * num;
-// });
+        name.innerHTML = `<input value="${name.textContent}">`;
+        surname.innerHTML = `<input value="${surname.textContent}">`;
+        salary.innerHTML = `<input value="${salary.textContent}">`;
 
-// console.log("Kvadratları:", squares);
+        e.target.textContent = "Yadda saxla";
+        e.target.classList.remove("edit");
+        e.target.classList.add("save");
+    }
 
+    else if(e.target.classList.contains("save")){
 
+        const row = e.target.closest("tr");
 
-// let arr5 = [10, 2, 6, 9];
+        row.children[1].textContent =
+            row.children[1].querySelector("input").value;
 
-// let sorted = arr5.sort(function(a, b) {
-//     return b - a;
-// });
+        row.children[2].textContent =
+            row.children[2].querySelector("input").value;
 
-// console.log("İkinci ən böyük:", sorted[1]);
+        row.children[3].textContent =
+            row.children[3].querySelector("input").value;
 
+        e.target.textContent = "Düzəliş et";
+        e.target.classList.remove("save");
+        e.target.classList.add("edit");
+    }
+});
 
+function updateNumbers(){
 
-// let arr6 = [10, 10, 10, 10];
+    const rows = tbody.querySelectorAll("tr");
 
-// let allSame = arr6.every(function(num) {
-//     return num === arr6[0];
-// });
-
-// console.log("Bütün elementlər eynidir?", allSame);
-
-
-
-
-
-
-
-// let user = ['john', 'doe', 25,20]
-// console.log(user);
-
-// let user ={
-//     username: 'jonh',
-//     lastname: 'doe',
-//     age: 25,
-//    'game level': 20,
-// }
-// user.salary = 2000;
-// user.level = 21;
-// delete user.username
-// console.log(user);
-// console.log(user.userAge);
-// console.log(user.lastname);
-// console.log(user['game level']);
-// console.log(user.salary);
-
-
-
-
-
-
-// let user = {
-//   name: "Maqsud",
-//   surname: "Ferecov",
-//   gender: "Kişi",
-//   birthday: "01.01.2000"
-// };
-
-// console.log(user.birthday);
-
-
-// class User {
-//     constructor(){
-//         this.username = "ertogrul";
-//         this.age = 20;
-
-//     }
-//     changeName(newName){
-//         this.username = newName
-//     }
-
-
-
-// }
-
-// // console.log(User);
-// let student1 = new User("ertogrul",20)
-// console.log(student1);
-// // console.log(student1.username);
-
-// let student2 = new User("mehdi")
-// console.log(student2);
-
-// class Person {
-//   constructor(surname, name, age, gender) {
-//     this.surname = surname;
-//     this.name = name;
-//     this.age = age;
-//     this.gender = gender;
-//   }
-//     celebrateBirthday() {
-//     this.age++;
-//     console.log(`${this.name} ad gününü qeyd etdi. Yeni yaş: ${this.age}`);
-// }
-// }
-
-
-// let person1 = new Person("Aliyev", "Murad", 25, "Kisi");
-// let person2 = new Person("Memmedova", "Aysel", 22, "Qadin");
-// let  person3 = new Person("Hüseynov", "Elvin", 30, "Kisi");
-
-// console.log(person1);
-// console.log(person2);
-// console.log(person3);
-
-
-
-
-
-
-
-
-
-
-// class Person {
-//   constructor(surname, name, age) {
-//     this.surname = surname;
-//     this.name = name;
-//     this.age = age;
-//   }
-// }
-
-
-// class Admin extends Person {
-//   constructor(surname, name, age, role, salary) {
-//     super(surname, name, age);
-//     this.role = role;
-//     this.salary = salary;
-//   }
-// }
-
-
-// class Player extends Person {
-//   constructor(surname, name, age, id) {
-//     super(surname, name, age);
-//     this.id = id;
-//   }
-// }
-
-
-// let admin1 = new Admin("Aliyev", "Murad", 30, "asif", 2500);
-
-// let player1 = new Player("Memmedov", "Rauf", 22, 101);
-
-// console.log(admin1);
-// console.log(player1);
-
-// let list = document.querySelectorAll(".primary");
-
-
-//     console.log("primary");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function addStudent(name) {
-    const ul = document.getElementById("studentList");
-
-    const li = document.createElement("li");
-    li.textContent = name;
-
-    
+    rows.forEach((row,index)=>{
+        row.children[0].textContent = index + 1;
+    });
 }
-
-addStudent("Emma Brillhart");
-addStudent("Luis Montes");
-addStudent("Dylan Schiemann");
-
-
